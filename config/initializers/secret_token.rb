e1480dfe98051e2d73ee1022aba7bb3fe73d5f48
boovius <joshua.book@gmail.com>
2013-07-15 13:05:48 -0700

    create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewRailsWPostGres::Application.config.secret_key_base = '9d7125c8793ad79687a155c40f0c1529351aca70daed14f5caec16f2ad7a3daaa9fbbcf2e56ebce451bd4c8d235499c3889e9466f287c3d23d04a327fb0ece1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewRailsWPostGres::Application.config.session_store :cookie_store, key: '_newRailsWPostGres_session'
